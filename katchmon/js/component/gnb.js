d0e53c3607248f64a437668552a9309ed59ae0d0
admin <admin@DA-PC-0098>
2020-11-06 16:38:38 +0900

    commit markup version 2

---
+++ katchmon/js/component/gnb.js
@@ -0,0 +1,17 @@
+Vue.component('katchmon-gnb-component', {
+	props: [],
+	data: function () {
+		return {
+			
+		}
+	},
+	methods: {
+		
+	},
+	created: function () {},
+	mounted: function () {},
+	template:
+		'<div class="katchmon-common-gnb-container">' + 
+			'<a class="katchmon-common-gnb-button-link-home" href="main.html">katchmon</a>' + 
+		'</div>'
+});
--- katchmon/js/component/question.js
+++ katchmon/js/component/question.js
@@ -1,44 +1,47 @@
 Vue.component('katchmon-question-component', {
-	props: [],
+	props: ['isShowQuestion'],
 	data: function () {
 		return {
-			isShowForm: false,
 			name: '',
 			email: '',
 			content: ''
 		}
 	},
 	methods: {
+		showQuestionFormHandler: function (isShow) {
+			this.isShowQuestion = isShow;
+			this.$emit('update:isShowQuestion', isShow);
+		},
 		sendQuestionPost: function () {
 			// TODO :: name, email, content 유효성 체크 / 비동기 접수 기능 구현 필요
 		}
 	},
-	created: function () {},
+	created: function () {
+		this.isShowQuestion = this.isShowQuestion !== undefined ? this.isShowQuestion : false;
+	},
 	mounted: function () {},
 	template: 
-		'<div class="katchmon-common-question-self">' + 
-			'<transition name="slide-fade-bottom">' + 
-				'<div v-if="isShowForm" class="katchmon-common-question-input-self">' + 
-					'<div class="katchmon-common-question-input-header">' + 
-						'<div class="katchmon-common-question-picture"></div>' + 
-						'<div class="katchmon-common-question-profile">' + 
-							'<p class="small-text">katchmon</p>' + 
-							'<p class="small-text">무엇이든 물어보세요!</p>' + 
-						'</div>' + 
-						'<button @click="isShowForm=false" class="katchmon-common-question-button-close"><span aria-hidden="true">×</span></button>' + 
+	'<div class="katchmon-common-question-self">' + 
+		'<transition name="slide-fade-bottom">' + 
+			'<div v-if="isShowQuestion" class="katchmon-common-question-input-self">' + 
+				'<div class="katchmon-common-question-input-header">' + 
+					'<div class="katchmon-common-question-picture"></div>' + 
+					'<div class="katchmon-common-question-profile">' + 
+						'<p class="small-text">katchmon</p>' + 
+						'<p class="small-text">무엇이든 물어보세요!</p>' + 
 					'</div>' + 
-					'<div class="katchmon-common-question-input-body">' + 
-						'<input v-model="name" type="text" class="katchmon-form-control" placeholder="이름" maxlength="20">' + 
-						'<input v-model="email" type="text" class="katchmon-form-control margin-top-10" placeholder="회신받으실 이메일" maxlength="50">' + 
-						'<textarea v-model="content" class="katchmon-form-control margin-top-10" placeholder="문의 내용"></textarea>' + 
-						'<div>' + 
-						'<button @click="sendQuestionPost()" class="katchmon-common-button katchmon-common-button-purple margin-top-20">문의하기</button>' + 
-						'</div>' + 
-					'</div>' + 
+					'<button @click="showQuestionFormHandler(false)" class="katchmon-common-question-button-close"><span aria-hidden="true">×</span></button>' + 
 				'</div>' + 
-			'</transition>' + 
-			'<transition name="slide-fade-bottom-onlyenter">' + 
-				'<button v-if="!isShowForm" @click="isShowForm=true" class="katchmon-common-question-button-open"></button>' + 
-			'</transition>' + 
-		'</div>'
+				'<div class="katchmon-common-question-input-body">' + 
+					'<input v-model="name" type="text" class="katchmon-form-control" placeholder="이름" maxlength="20">' + 
+					'<input v-model="email" type="text" class="katchmon-form-control margin-top-10" placeholder="회신받으실 이메일" maxlength="50">' + 
+					'<textarea v-model="content" class="katchmon-form-control margin-top-10" placeholder="문의 내용"></textarea>' + 
+					'<button @click="sendQuestionPost()" class="katchmon-common-button katchmon-common-button-purple margin-top-20">문의하기</button>' + 
+				'</div>' + 
+			'</div>' + 
+		'</transition>' + 
+		'<transition name="slide-fade-bottom-onlyenter">' + 
+			'<button v-if="!isShowQuestion" @click="showQuestionFormHandler(true)" class="katchmon-common-question-button-open"></button>' + 
+		'</transition>' + 
+	'</div>'
 });
